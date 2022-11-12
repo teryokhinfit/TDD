fe568b02affc762dc59f86470a19d6a404325492
teryokhinfit <terekhincw@gmail.com>
2022-11-12 13:54:00 +0400

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,6 @@
 public class PhoneBook {
+
+    public boolean add(String name, String number) {
+        return false;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,28 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+
 public class PhoneBookTest {
+
+    @BeforeAll
+    public static void started () {
+        System.out.println("test started");
+    }
+
+    @AfterAll
+    public static void finishedAll () {
+        System.out.println("all test completed");
+    }
+
+    @Test
+    public void addContact() {
+        PhoneBook phoneBook = new PhoneBook();
+        String name = "Vasya";
+        String number = "+7 912 345 67 89";
+        assertThat(phoneBook.add(name, number)).isTrue();
+    }
+
+
 }
